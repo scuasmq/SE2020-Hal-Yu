952acc4f71c061c673970a2f48cd05d7085717b5
smq <asongmingqing@qq.com>
2020-11-09 11:17:35 +0800

    add db_insert

---
+++ conndb.py
@@ -0,0 +1,12 @@
+import pymysql
+conn = pymysql.connect(
+    host = "localhost",
+    user = "dataUser",
+    password = "scusmq61347",
+    database = "SE2020",
+    charset = "utf8"
+)
+cursor =  conn.cursor()
+sql = 'select count(*) from goldennum'
+cursor.execute(sql)
+print(cursor.fetchone())
